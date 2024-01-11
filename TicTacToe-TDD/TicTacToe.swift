582bd005fe90f6af0acdd0c580986c33df111122
Resn4R <purview-joust.0x@icloud.com>
2024-01-11 19:11:05 +0000

    1 test passed

---
+++ TicTacToe-TDD/TicTacToe.swift
@@ -0,0 +1,22 @@
+//
+//  TicTacToe.swift
+//  TicTacToe-TDD
+//
+//  Created by Vito Borghi on 11/01/2024.
+//
+
+import Foundation
+
+struct TicTacToe {
+    
+    private var grid = [[""]]
+    
+    func getGrid() -> [[String]] {
+        grid
+    }
+    
+    func place(symbol: String, in location: (Int, Int)) {
+    
+    
+
+}
--- TicTacToe-TDDTests/TicTacToe_TDDTests.swift
+++ TicTacToe-TDDTests/TicTacToe_TDDTests.swift
@@ -9,28 +9,21 @@ import XCTest
 @testable import TicTacToe_TDD
 
 final class TicTacToe_TDDTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //x goes first
+    //alternate x n o
+    //cannot play on occupied positions
+    //3 x or 3 o wins
+    //all boxes full and no win is a draw
+    
+    func testSpawnGrid() {
+        let sample = TicTacToe()
+        
+        XCTAssertEqual(sample.getGrid(), [[""]])
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testADDSymbolToGrid() {
+        let sampleGrid = TicTacToe().spawnGrid()
+        
+        let symbol = "X"
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

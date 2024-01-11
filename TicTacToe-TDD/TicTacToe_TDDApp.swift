e33a8efabcc6e68108f883b2bfd873722c2c675f
Resn4R <purview-joust.0x@icloud.com>
2024-01-11 18:48:26 +0000

    Initial Commit

---
+++ TicTacToe-TDD/TicTacToe_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TDDApp.swift
+//  TicTacToe-TDD
+//
+//  Created by Vito Borghi on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
